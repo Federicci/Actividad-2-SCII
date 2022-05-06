0e8ba563c6d72cefff3ba53d7f4dcfa2baf40d57
Julián Pucheta <82587926+Julianpucheta@users.noreply.github.com>
2022-05-06 20:15:37 -0300

    Update PENDULO_3.m

--- PENDULO_3.m
+++ PENDULO_3.m
@@ -1,4 +1,4 @@
-clear all; close all;
+cclear all; close all;
 
 m=.1;
 Fricc=0.1; 
@@ -6,13 +6,37 @@ l=0.6;
 g=9.8;
 M=.5;
 
+
 %Linealizado en el equilibrio estable:
-A=[0 1 0 0;0 -Fricc/M -m*g/M 0; 0 0 0 1; 0 Fricc/(l*M) -g*(m+M)/(l*M) 0];
-B=[0; 1/M; 0; -1/(l*M)];
+A=[0 1 0 0;0 -Fricc/M -m*g/M 0; 0 0 0 1; 0 -Fricc/(l*M) -g*(m+M)/(l*M) 0];
+B=[0; 1/M; 0; 1/(l*M)];
 C=[1 0 0 0]; 
+% / 0,     1,         0,      0 \
+% |                             |
+% |       Fricc       g m       |
+% | 0,  - -----,    - ---,    0 |
+% |         M          M        |
+% |                             |
+% | 0,     0,         0,      1 |
+% |                             |
+% |       Fricc    g (M + m)    |
+% | 0, - ------, - ---------, 0 |
+% \      M long      M long     /
+% 
+% /    0   \
+% |        |
+% |    1   |
+% |    -   |
+% |    M   |
+% |        |
+% |    0   |
+% |        |
+% |    1   |
+% | ------ |
+% \ M long /
 
 %Diseño con LQR
-Q=10*diag([1 1 10000 1]);    R=.1;
+Q=1*diag([10 1 1 1]);    R=100;
 %Hamiltoniano
 H=[A -B*inv(R)*B'; -Q -A'];
 [vects1,autovals1]=eig(H);  %columnas de vects: autovectores
@@ -37,8 +61,8 @@ K=inv(R)*B'*P1;
 G=-inv(C*inv(A-B*K)*B);
 
 %Simulación del control:
-deltat=10^-4;
-ts=20;
+deltat=10^-3;
+ts=50;
 pasos=round(ts/deltat);
 Ci=[0 0 pi 0];
 t=0:deltat:(ts-deltat);
@@ -55,19 +79,18 @@ x(2,1)=Ci(2);
 x(3,1)=Ci(3);
 x(4,1)=Ci(4);
 ua(1)=0;
-
+xOP=[0;0;pi;0];
 for i=2:1:pasos
     x_actual=x(:,i-1);
     u_actual=-K*x_actual+ref_dist(i-1)*G;
     ua=[ua u_actual];
-    
-    x_p_1=x_actual(2);
-    x_p_2=-Fricc*x_actual(2)/M-m(i-1)*g*x_actual(3)/M+u_actual/M;
-    x_p_3=x_actual(4);
-    x_p_4=Fricc*x_actual(2)/(l*M)-g*(m(i-1)+M)*x_actual(3)/(l*M)-u_actual/(l*M);
-    x_p_actual=[x_p_1; x_p_2; x_p_3; x_p_4];
-    
-    x_sig=x_actual+deltat*x_p_actual;
+    x_p=A*(x_actual-xOP)+B*u_actual;
+%     x_p_1=x_actual(2);
+%     x_p_2=-Fricc*x_actual(2)/M-m(i-1)*g*x_actual(3)/M+u_actual/M;
+%     x_p_3=x_actual(4);
+%     x_p_4=Fricc*x_actual(2)/(l*M)-g*(m(i-1)+M)*x_actual(3)/(l*M)-u_actual/(l*M);
+%     x_p_actual=[x_p_1; x_p_2; x_p_3; x_p_4];    
+    x_sig=x_actual+deltat*x_p;
     x(:,i)=x_sig;
 end
 
